2c4897865cb56a143ef9ecd5609f78b158e7257f
2001056 <119822421+2001056@users.noreply.github.com>
2025-11-27 17:20:21 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for (int num : nums) {
+            if (isPrime(num)) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
