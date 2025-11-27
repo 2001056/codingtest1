e1656649408b816836734f60ac55f8ac923c20bb
2001056 <119822421+2001056@users.noreply.github.com>
2025-11-27 17:29:45 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String[] nums = br.readLine().split(" ");
+            int A = Integer.parseInt(nums[0]);
+            int B = Integer.parseInt(nums[1]);
+            System.out.println(A + B);
+        }
+    }
+}
