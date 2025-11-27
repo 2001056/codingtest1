0d93182ac044c7afd05a6881a5ea47d61979332d
2001056 <119822421+2001056@users.noreply.github.com>
2025-11-27 17:22:17 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = -1;
+        int index = -1;
+
+        for ( int i =1;i <=9;i++){
+            int num = Integer.parseInt(br.readLine());
+            if (num > max) {
+                max = num;
+                index = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+
+
+
+
+
+
+    }
+}
