99e2a797a2cab3d878cd64d0f929a284ba082f57
2001056 <119822421+2001056@users.noreply.github.com>
2025-11-27 17:21:47 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+        int d = Integer.parseInt(arr[3]);
+        int e = Integer.parseInt(arr[4]);
+        System.out.println((a*a+b*b+c*c+d*d+e*e)%10);
+
+    }
+}
