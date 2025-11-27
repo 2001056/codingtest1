4d006955ae6e176d7ea7bb012323b72cfa846fbe
2001056 <119822421+2001056@users.noreply.github.com>
2025-11-27 17:27:34 +0900

    [Bronze III] Title: ACM 호텔, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int floor = (N - 1) % H + 1;
+            int room = (N - 1) / H + 1;
+            sb.append(floor * 100 + room).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
