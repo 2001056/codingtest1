b815030fa1ac668939600b53321eb302da67739b
2001056 <119822421+2001056@users.noreply.github.com>
2025-12-12 16:39:29 +0900

    [Silver IV] Title: 베스트셀러, Time: 204 ms, Memory: 17920 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,54 @@
+
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] arr = new String[n];
+        Map<String, Integer> myMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextLine();
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            myMap.put(arr[i], myMap.getOrDefault(arr[i], 0) + 1);
+
+        }
+
+        String maxKey = "";
+
+        int max = Integer.MIN_VALUE;
+        for (String a : myMap.keySet()) {
+            int value = myMap.get(a);
+            if (max < value) {
+                max = value;
+                maxKey = a;
+
+            }
+            for (String b: myMap.keySet()){
+                if (value > max || (value == max && a.compareTo(maxKey) < 0)) {
+                    max = value;
+                    maxKey = a;
+                }
+            }
+        }
+
+        System.out.println(maxKey);
+
+    }
+
+
+
+
+
+
+    }
+
